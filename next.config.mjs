9c2b0373b2e882c45c85fb47ba16f4f5862089b7
Harsh <kaushikyuvraj134@gmail.com>
2025-08-06 00:13:29 +0530

    add about section

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+    domains: ['images.unsplash.com', 'raw.githubusercontent.com'],
+  },
+};
 
 export default nextConfig;
--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+import About from "@/components/About";
 import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
@@ -9,6 +10,7 @@ export default function Home() {
     <>
     <div className="lex items-center flex-col justify-between bg-[url('https://raw.githubusercontent.com/prebuiltui/prebuiltui/main/assets/hero/bg-gradient-2.png')] bg-cover text-sm text-white max-md:px-4 text-center bg-gray-900 ">
     <Hero/>
+    <About/>
     <Testimonial/>
     <Footer/>
     </div>
