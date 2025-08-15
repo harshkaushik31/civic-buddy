22d2a75d99d11c7e8d09f8eb0da6880a30fc3b49
Harsh <kaushikyuvraj134@gmail.com>
2025-08-15 16:37:33 +0530

    modify db connection

--- src/utils/connectDB.js
+++ src/utils/connectDB.js
@@ -1,18 +1,8 @@
-import mongoose from 'mongoose'
-
-let isConnected = false;
+import mongoose from 'mongoose';
 
 export const connectDB = async () => {
-
-    if(isConnected){
-        console.log('MongoDB already connected');
-        return; 
-    }
-
     try{
         const connection = await mongoose.connect(process.env.MONGODB_URI);
-        
-        isConnected = conn.connections[0].readyState;
         console.log("MongoDB connected successfully");
     }catch(error){
         console.log("Could Not connect to DB: ",error.message);
