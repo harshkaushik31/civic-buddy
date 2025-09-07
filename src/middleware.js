a0dbbbfb9530269e54ed7fe66932078ed2d19242
Harsh <kaushikyuvraj134@gmail.com>
2025-09-07 11:59:13 +0530

    add middleware but requires some work

--- src/middleware.js
+++ src/middleware.js
@@ -1,6 +1,5 @@
 import { NextResponse } from 'next/server'
 
-// This function can be marked `async` if using `await` inside
 export function middleware(request) {
   const path = request.nextUrl.pathname
   
